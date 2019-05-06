6f996719db4f479d8c66130936726af903b7425d
Richard Turner <rclt201@gmail.com>
2019-05-06 12:23:07 +0100

    changed the selection functionality so it disables the product selection when a company is not selected

--- src/app.js
+++ src/app.js
@@ -6,141 +6,155 @@ import axios from 'axios'
 import { BrowserRouter as Browser } from 'react-router-dom'
 
 class App extends React.Component {
-    constructor() {
-        super()
-        this.state = {}
+  constructor() {
+    super()
+    this.state = {}
 
-        this.handleChange = this.handleChange.bind(this)
-    }
+    this.handleSupplier = this.handleSupplier.bind(this)
+    this.handleProduct = this.handleProduct.bind(this)
+  }
 
-    componentDidMount() {
-        axios
-        .all([axios.get(`/api/suppliers`), axios.get("/api/products")])
-        .then(res => {
-            const [suppliers, products] = res
-            const uniqueProducts = Array.from(
-            new Set(products.data.map(product => product.name))
-            ).map(name => {
-            return products.data.find(product => product.name === name)
-            })
-            this.setState({ suppliers, uniqueProducts, products })
-        })
-    }
+  componentDidMount() {
+    axios
+      .get(`/api/suppliers`)
+      .then(res => this.setState({ suppliers: res.data }))
+  }
 
-    updateResults() {
-        if (
-            this.state.data &&
-            this.state.data.product &&
-            this.state.data.supplier
-        ) {
-        let productResult = this.state.products.data.filter(
-        product => {
-            return (
-            product.name === this.state.data.product &&
-            product.supplier._id === this.state.data.supplier
-            )
-        })
-        if (productResult.length !== 0) {
-            let result = {
-            ...this.state.result,
-            product: productResult[0].name,
-            supplier: productResult[0].supplier.name,
-            price: productResult[0].price,
-            id: productResult[0]._id
-            }
-            this.setState({ result })
-            } else {
-            let result = {}
-            this.setState({ result })
-            }
-        }
-    }
+  handleSupplier(e) {
+    let result = {}
+    let products = {}
+    let selection = e.target
+    let data = { ...this.state.data, result, products, supplier: selection.value }
+    this.setState({ data }, () => {
+      this.updateResults()
+    })
+    axios.get(`/api/products`).then(res => {
+      let products = res.data.filter(product => {
+        return product.supplier._id === selection.value
+      })
+      this.setState({ products: products })
+    })
+  }
 
-    handleChange(e) {
-        let selection = e.target
-        let data = { ...this.state.data, [selection.name]: selection.value }
-        this.setState({ data }, () => {
-            this.updateResults()
-        })
-    }
+  handleProduct(e) {
+    let selection = e.target
+    let data = { ...this.state.data, product: selection.value }
+    this.setState({ data }, () => {
+      this.updateResults()
+    })
+  }
 
-    render() {
-        console.log("state", this.state)
+  updateResults() {
+    if (this.state.data.product && this.state.data.supplier) {
+      let productResult = this.state.products.filter(product => {
         return (
-        <Browser>
-            <main>
-            <div className="container-fluid">
-                <div className="row">
-                <div className="col-sm-12 col-md-12 main">
-                    <h1 className="page-header">Product pricing</h1>
-                    <form>
-                    <div className="row">
-                        <div className="form-group col-md-6">
-                        <label htmlFor="selSupplier">Supplier</label>
+          product.name === this.state.data.product &&
+          product.supplier._id === this.state.data.supplier
+        )
+      })
+      if (productResult.length !== 0) {
+        let result = {
+          ...this.state.result,
+          product: productResult[0].name,
+          supplier: productResult[0].supplier.name,
+          price: productResult[0].price,
+          id: productResult[0]._id
+        }
+        this.setState({ result })
+      } else {
+        let result = {}
+        this.setState({ result })
+      }
+    }
+  }
+
+  render() {
+    return (
+      <Browser>
+        <main>
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-sm-12 col-md-12 main">
+                <h1 className="page-header">Product pricing</h1>
+                  <div className="row">
+                    <div className="form-group col-md-6">
+                      <label htmlFor="selSupplier">Supplier</label>
+                      <select
+                        className="form-control"
+                        id="selSupplier"
+                        onChange={this.handleSupplier}
+                        name="supplier"
+                      >
+                        <option value="x">Select Supplier</option>
+                        {this.state.suppliers &&
+                          this.state.suppliers.map(supplier => (
+                            <option key={supplier._id} value={supplier._id}>
+                              {supplier.name}
+                            </option>
+                          ))}
+                      </select>
+                    </div>
+                    <div className="form-group col-md-6">
+                      <label htmlFor="selProduct">Product</label>
+                      {!this.state.products || this.state.products.length === 0 ? (
                         <select
-                            className="form-control"
-                            id="selSupplier"
-                            onChange={this.handleChange}
-                            name="supplier"
-                        >
-                            <option value="x">Select Supplier</option>
-                            {this.state.suppliers &&
-                            this.state.suppliers.data.map(supplier => (
-                                <option key={supplier._id} value={supplier._id}>
-                                {supplier.name}
-                                </option>
-                            ))}
-                        </select>
-                        </div>
-                        <div className="form-group col-md-6">
-                        <label htmlFor="selProduct">Product</label>
+                          className="form-control"
+                          id="selProduct"
+                          onChange={this.handleProduct}
+                          name="product"
+                          disabled
+                        />
+                      ) : (
                         <select
                             className="form-control"
                             id="selProduct"
-                            onChange={this.handleChange}
+                            onChange={this.handleProduct}
                             name="product"
                         >
                             <option value="x">Select Product</option>
-                            {this.state.uniqueProducts &&
-                            this.state.uniqueProducts.map(product => (
-                                <option key={product._id} value={product.name}>
+                            {this.state.products &&
+                            this.state.products.map(product => (
+                                <option
+                                key={product._id}
+                                value={product.name}
+                                >
                                 {product.name}
                                 </option>
                             ))}
                         </select>
-                        </div>
+                          )}
                     </div>
-                    </form>
-                    <h2 className="sub-header">Product details</h2>
-                    <div className="table-responsive">
-                    <table className="table table-striped">
-                        <thead>
+                  </div>
+                <h2 className="sub-header">Product details</h2>
+                <div className="table-responsive">
+                  <table className="table table-striped">
+                    <thead>
+                      <tr>
+                        <th>#</th>
+                        <th>Supplier</th>
+                        <th>Product</th>
+                        <th>Price</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {this.state.result && (
                         <tr>
-                            <th>#</th>
-                            <th>Supplier</th>
-                            <th>Product</th>
-                            <th>Price</th>
+                          <td>{this.state.result.id}</td>
+                          <td>{this.state.result.supplier}</td>
+                          <td>{this.state.result.product}</td>
+                          <td>{this.state.result.price}</td>
                         </tr>
-                        </thead>
-                        <tbody>
-                        {this.state.result && (
-                            <tr>
-                            <td>{this.state.result.id}</td>
-                            <td>{this.state.result.supplier}</td>
-                            <td>{this.state.result.product}</td>
-                            <td>{this.state.result.price}</td>
-                            </tr>
-                        )}
-                        </tbody>
-                    </table>
-                    </div>
+                      )}
+                    </tbody>
+                  </table>
                 </div>
-                </div>
+              </div>
             </div>
-            </main>
-        </Browser>
-        )
-    }
+          </div>
+        </main>
+      </Browser>
+    )
+  }
 }
 
 ReactDOM.render(
